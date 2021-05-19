54d2c6ea761ba0d7dc6e2c05763ae81a4d05e0f1
Kauan Santos <kauansantos83@gmail.com>
2021-05-19 19:29:08 -0300

    adicionado alterações no form

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,58 +8,71 @@ class UserController {
     }
 
     onSubmit() {
-        
+
         this.formEl.addEventListener("submit", event => {
+            
             event.preventDefault();
+            
+            let btn = this.formEl.querySelector('[type=submit]')
+        
+            btn.disabled = true;
+
+
             let values = this.getValues();
 
-            this.getPhotos.then(
-                function(content) {
+            this.getPhotos().then(
+                (content) => {
                     values.photo = content;
 
                     this.addLine(values);
 
-            }, 
-            function(e){
+                    this.formEl.reset();
+
+                    btn.disabled = false;
+                },
+
+                (e) => {
 
                     console.log(error);
                 }
-            );   
-         
+            );
+
         });
     };
 
 
     getPhotos() {
 
-        return Promise(function(resolve, reject){
+        return new Promise((resolve, reject) => {
 
-        
+            let fileReader = new FileReader();
 
-        let fileReader = new FileReader();
+            let elements = [...this.formEl.elements].filter(item => {
 
-        let elements = [...this.formEl.elements].filter(item=> {
+                if (item.name === 'photo') {
+                    return item;
+                }
+            })
 
-            if (item.name === 'photo') {
-                return item;
+            let file = elements[0].files[0]
+
+            fileReader.onload = () => {
+
+                resolve(fileReader.result)
+
+            };
+
+            fileReader.onerror = (e) => {
+
+                reject(e);
+
+            };
+
+            if (file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
             }
-        });
-
-        let file = elements[0].files[0]
-
-        fileReader.onload = () => {
-
-            resolve(fileReader.result)
-
-        };
-
-        fileReader.onerror = (e) => {
-
-            reject(e);
-
-        };
-
-        fileReader.readAsDataURL(file);
 
         })
     }
@@ -78,6 +91,10 @@ class UserController {
                     user[fields.name] = fields.value;
                 }
 
+            } else if(fields.name == 'admin' ) { 
+            
+                user[fields.name] = fields.checked;
+
             } else {
                 user[fields.name] = fields.value;
             }
@@ -93,25 +110,26 @@ class UserController {
     }
 
 
-    
+
     addLine(dataUser) {
-    
-    this.tableEl.innerHTML = 
-    `<tr>
+
+        let tr = document.createElement('tr');
+
+        tr.innerHTML =
+        `
         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
+        <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
         <td>${dataUser.birth}</td>
         <td>
         <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
         <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
         </td>
-  </tr>`
-}
+  `
 
-
-
-
+        this.tableEl.appendChild(tr); 
+  
+    }
 
 }
